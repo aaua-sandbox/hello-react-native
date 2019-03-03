9cbd89f8c5bafede53d70367650833c6aa6b8d96
aaua <15936438+aaua@users.noreply.github.com>
2019-03-03 13:50:02 +0900

    add comment

--- screens/BaseScreen.js
+++ screens/BaseScreen.js
@@ -4,6 +4,7 @@ import BaseComponent from '../components/BaseComponent'
 
 export default class BaseScreen extends BaseComponent {
   componentWillUpdate() {
+    // https://kang.hateblo.jp/entry/2018/10/20/234246
     LayoutAnimation.easeInEaseOut()
   }
 }
